7dda93d2e0db616c85ebc2b9934d0574ebb88b3b
Max Barkley <max.barkley@gmail.com>
2021-02-15 11:48:05 -0500

    First working liquibase/sqlite setup

--- src/main/java/io/github/mbarkley/rollens/Main.java
+++ src/main/java/io/github/mbarkley/rollens/Main.java
@@ -2,10 +2,18 @@ package io.github.mbarkley.rollens;
 
 import io.github.mbarkley.rollens.parse.Parser;
 import io.github.mbarkley.rollens.util.EnvUtil;
+import liquibase.Contexts;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.ClassLoaderResourceAccessor;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.jdbi.v3.core.Handle;
 import org.jdbi.v3.core.Jdbi;
 import org.jdbi.v3.sqlite3.SQLitePlugin;
 import org.jdbi.v3.sqlobject.SqlObjectPlugin;
@@ -26,6 +34,16 @@ public class Main {
     Jdbi jdbi = Jdbi.create("jdbc:sqlite:" + dbFile)
                     .installPlugin(new SqlObjectPlugin())
                     .installPlugin(new SQLitePlugin());
+    try (Handle handle = jdbi.open()) {
+      final Database db = DatabaseFactory.getInstance()
+                                         .findCorrectDatabaseImplementation(new JdbcConnection(handle
+                                                                                                   .getConnection()));
+      final Liquibase liquibase = new Liquibase("/changelog.yaml", new ClassLoaderResourceAccessor(), db);
+      liquibase.update(new Contexts());
+      handle.commit();
+    } catch (LiquibaseException e) {
+      throw new IllegalStateException("Unable to initialize bot DB", e);
+    }
 
     // We only need 2 intents in this bot. We only respond to messages in guilds and private channels.
     // All other events will be disabled.
